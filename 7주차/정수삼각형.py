a99abe7fed00f29aedcd61c6420debed749f7f4a
tjdgus <tjdgus1977@gmail.com>
2022-06-26 21:12:45 +0900

    add: 정수삼각형

--- 7주차/정수삼각형.py
+++ 7주차/정수삼각형.py
@@ -4,17 +4,17 @@ def solution(triangle):
     width = height = 0
     for i in intList:
         for j in i:
-            if height == 0 and width == 0: # 가장 처음 합은 더할 값이 없기 때문에 자기 자신
-                SUM[height][width] = j
+            if height == 0 and width == 0: # 가장 처음 합
+                SUM[height][width] = j # 윗줄이 없어서 더할 값이 없기 때문에 자기 자신 저장
             elif width == 0: # 만약 맨 왼쪽이라면
-                SUM[height][width] = SUM[height-1][width] + j # 그 위의 값 가져와서 자기 자신과의 합
-            elif width == (len(i)-1):
-                SUM[height][width] = SUM[height-1][width-1] + j # 
-            else:
-                SUM[height][width] = max(SUM[height-1][width] + j, SUM[height-1][width-1] + j)
+                SUM[height][width] = SUM[height-1][width] + j # 그 윗줄의 가장 왼쪽 값 가져와서 자기 자신과의 합 저장
+            elif width == (len(i)-1): # 만약 맨 오른쪽이라면
+                SUM[height][width] = SUM[height-1][width-1] + j # 그 윗줄의 가장 오른쪽 값 가져와서 자기 자신과의 합 저장
+            else: # 그외 나머지
+                SUM[height][width] = max(SUM[height-1][width] + j, SUM[height-1][width-1] + j) # 좌우 경로에 따른 값 중 큰 값 저장
             width +=1
         height   +=1
-        width = 0
+        width = 0 # 너비 초기화
 
     return max(map(max,SUM))
 
